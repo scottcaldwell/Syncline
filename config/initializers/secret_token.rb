4a9a37f944d7b2bb251f52655af676a61f4d2efd
Scott Caldwell <scottc.caldwell@gmail.com>
2016-03-18 00:50:41 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syncline::Application.config.secret_key_base = '6bcb15a21eaf078c2a5dc75c50a628db8c01f78bfb0a6feffe9254c2156592fa7196d2dd6cf9b2d653e59c5890e6538519aa3f42ea5544e1620f65d84a236eba'
