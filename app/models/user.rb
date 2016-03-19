cdbde4ecdeab3383b0e6a0b287b79957d10dfa62
vagrant <vagrant@lighthouse-labs>
2016-03-19 01:07:08 +0000

    added password_digest to users table, added has_secure_password to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :password,
+    length: { in: 6..20 }, on: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318011946) do
+ActiveRecord::Schema.define(version: 20160319010406) do
 
   create_table "drill_holes", force: true do |t|
     t.string   "name"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20160318011946) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
