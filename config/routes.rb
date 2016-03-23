44aaf74f44e1d270b4884ba54c1170259a91c069
quicklikerabbit <steven.d.richards@gmail.com>
2016-03-23 22:58:56 +0000

    Formatting changes to login forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Syncline::Application.routes.draw do
       resources :layers
     end
   end
-  resource :session, only: [:create, :destroy, :new]
+  resource :sessions, only: [:create, :destroy, :new]
   resources :users, only: [:new, :show, :create, :edit, :destroy]
   root to: 'sites#index'
   # The priority is based upon order of creation: first created -> highest priority.
