b334b3b6af7769d59e6152d44e6a410bbe9cefa4
quicklikerabbit <steven.d.richards@gmail.com>
2016-03-25 02:25:37 +0000

    Added field test and lab test controllers, added create methods and protected params methods

--- app/controllers/field_tests_controller.rb
+++ app/controllers/field_tests_controller.rb
@@ -1,7 +1,12 @@
 class FieldTestsController < ApplicationController
 
   def create
-    @field_test = FieldTest.create()
+    @field_test = FieldTest.new(field_test_params)
+    if field_test.save
+      respond_to do |format|
+        format.json { render json: { data: @field_test } }
+      end
+    end
   end
 
   def new
@@ -16,7 +21,7 @@ class FieldTestsController < ApplicationController
   protected
 
   def field_test_params
-    params.require(:field_test).permit(:depth_from, :depth_to, :test_type, :layer_id)
+    params.require(:field_test).permit(:depth_from, :depth_to, :test_type)
   end
 
 
