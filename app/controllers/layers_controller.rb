44951de87cd3e85bcffd039e69e7a316783f069a
quicklikerabbit <steven.d.richards@gmail.com>
2016-03-28 21:08:01 +0000

    added comma to fix error

--- app/controllers/layers_controller.rb
+++ app/controllers/layers_controller.rb
@@ -6,7 +6,7 @@ class LayersController < ApplicationController
       drill_hole_id: params[:drill_hole_id],
       thickness: params[:thickness],
       description: params[:description],
-      material_type_id: params[:material_type_id]
+      material_type_id: params[:material_type_id],
       date_drilled: params[:date_drilled]
     )
     session[:layer_id] = @layer.id
