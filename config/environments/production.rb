65d4597d83bb8743af47b49528a7141f751be365
quicklikerabbit <steven.d.richards@gmail.com>
2016-03-30 23:00:41 +0000

    changed gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,11 @@ Syncline::Application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['DOMAIN'],
-    :user_name      => ENV['USERNAME'],
-    :password       => ENV['PASSWORD'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
